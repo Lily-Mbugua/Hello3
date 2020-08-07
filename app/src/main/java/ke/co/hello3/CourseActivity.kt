4dbe438f649ce8494df70050c8d8b69aa80b8846
Lily Mbugua <wanjerimbugua07@gmail.com>
2020-08-07 16:24:03 +0300

    first commit

---
+++ app/src/main/java/ke/co/hello3/CourseActivity.kt
@@ -0,0 +1,11 @@
+package ke.co.hello3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course)
+    }
+}
